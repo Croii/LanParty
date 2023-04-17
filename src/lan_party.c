599cfdc314c3467604476735b230c18acbc451a4
Croii <reversalrege@yahoo.com>
2023-04-17 17:30:08 +0300

    using switch

--- src/lan_party.c
+++ src/lan_party.c
@@ -1,5 +1,7 @@
-#include "../include/player.h"
-#include "../include/team.h"
+#include "..//include//player.h"
+#include "..//include//team.h"
+#include "..//include//match.h"
+
 
 #include <math.h>
 #include <stdio.h>
@@ -12,12 +14,14 @@
 void computeScores(TeamNode *head) {
 
     PlayerNode *player = head->players;
-    int score = 0;
+    float score = 0;
+    int numberOfPlayers = 0;
     while (player != NULL) {
         score += player->points;
         player = player->nextPlayer;
+        numberOfPlayers++;
     }
-    head->score = score;
+    head->score = score / numberOfPlayers;
 }
 
 void computeAllScores(TeamNode *head) {
@@ -35,46 +39,57 @@ int main(int argc, char **argv) {
     char *outputFilePath = argv[3];
 
     //reading tasks from file
-    int tasks[TASKS];
+    int taskToExecute = 0;
+    int aux;
     FILE *taskFile = fopen(tasksFilePath, "rt");
-    for (int i = 0; i < TASKS; i++)
-        fscanf(taskFile, "%d", &tasks[i]);
+    for (int i = 0; i < TASKS; i++) {
+        fscanf(taskFile, "%d", &aux);
+        taskToExecute += aux;
 
-    //finding the right task to execute
-    int TASK1 = tasks[0] == 1 && tasks[1] == 0 && tasks[2] == 0 && tasks[3] == 0 && tasks[4] == 0;
-    int TASK2 = tasks[0] == 1 && tasks[1] == 1 && tasks[2] == 0 && tasks[3] == 0 && tasks[4] == 0;
-    int TASK3 = tasks[0] == 1 && tasks[1] == 1 && tasks[2] == 1 && tasks[3] == 0 && tasks[4] == 0;
-    int TASK4 = tasks[0] == 1 && tasks[1] == 1 && tasks[2] == 1 && tasks[3] == 1 && tasks[4] == 0;
-    int TASK5 = tasks[0] == 1 && tasks[1] == 1 && tasks[2] == 1 && tasks[3] == 1 && tasks[4] == 1;
-
-    // clearing file input
-    fclose(fopen(outputFilePath, "wt"));
-    TeamNode *teams = NULL;
-    readTeams(&teams, teamsFilePath);
-
-     if (TASK1) {
-        showTeams(teams, outputFilePath);
-    }  
-
-     if (TASK2) {
-        computeAllScores(teams);
-        removeTeams(&teams, teamsFilePath);
-        showTeams(teams, outputFilePath);
     }
-
-    if (TASK3) {
-    }
-
-    if (TASK4) {
-    }
-
-    if (TASK5) {
-    }
+    
+    fclose(taskFile);
 
     
-        computeAllScores(teams);
-        removeTeams(&teams, teamsFilePath);
-       // showTeams(teams, 1);
+    
+    
+    // clearing file input
+    fclose(fopen(outputFilePath, "wt"));
+
+
+    TeamNode *teams = NULL;
+    int numberOfTeams = 0;
+    
+
+    readTeams(&teams, teamsFilePath, &numberOfTeams);
+    switch(taskToExecute) {
+        case 1:
+            showTeams(teams, outputFilePath);
+            break;
+        case 2:
+            computeAllScores(teams);
+            removeTeams(&teams, teamsFilePath, &numberOfTeams);
+            showTeams(teams, outputFilePath);
+
+            break;
+        case 3:
+
+            TeamsQueue *teamsQueue = NULL;
+            computeAllScores(teams);
+            removeTeams(&teams, teamsFilePath, &numberOfTeams);
+            showTeams(teams, outputFilePath);
+            teamsQueue = createQueueTeam();
+            //showTeams(teams, outputFilePath);
+            preparingMatches(teamsQueue, &teams);
+            simulatingMatches(teamsQueue, &numberOfTeams, outputFilePath);
+            break;
+        case 4:
+            break;
+        case 5:
+            break;
+}
+    
+
+        //freeTeams(&teams);
         //free(*argv);
-        printf("1"); 
 }
